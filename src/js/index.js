259558da3a96d28b6cd4b7d17aa94c8f4ff45584
Tejas Vaij <intern.tejas@sangnak.in>
2022-04-10 13:32:20 +0530

    added textarea (no saving)

--- src/js/index.js
+++ src/js/index.js
@@ -85,44 +85,45 @@ const label = document.createElement("label");
 
 //take picture function
 function takePicture() {
+
+  // Save Image
   var picture = webcam.snap();
-
   let dir = localStorage.getItem("dir");
+  saveImage(picture, dir);
 
+  // Add Card in Image/Video List Area
   let h4 = document.createElement("h4");
-  // fname = fname.value;
   h4.innerText = `patient name : ${fname}`;
+
   let img = document.createElement("img");
   let div = document.createElement("div");
   let div1 = document.createElement("div");
+
   img.src = picture;
-
-  saveImage(picture, dir);
-
   img.alt = "image/png";
   div1.classList.add("img-container");
   div.appendChild(img);
+
   let p = document.createElement("p");
   p.innerText = `Time : ${today.toLocaleTimeString()}`;
+
+  let div_text_input = document.createElement("div");
+  div_text_input.classList.add("d-flex");
+  div_text_input.classList.add("justify-content-center");
+  let text_input = document.createElement("textarea");
+  text_input.classList.add("mt-1")
+  text_input.placeholder = "note for image"
+  div_text_input.appendChild(text_input)
+
   div1.appendChild(h4);
   div1.appendChild(p);
   div1.appendChild(div);
+  div1.appendChild(div_text_input);
+
   imageList.appendChild(div1);
 }
 
-// function getDateTime() {
-//   var today = new Date();
-//   var todaysDate = today.toDateString().split(" ").join("_");
-//   var todaysTime = today.toTimeString().split(" ");
-//   var time = todaysTime[0].split(":").join("_");
-//   return todaysDate.concat("_" + time);
-// }
-
 function saveImage(picture, dir) {
-  // var link = document.getElementById('id-1');
-  // // link.download = "test.png";
-  // link.href = picture;
-  // const fileContents = new Buffer(picture, "base64");
   var matches = picture.match(/^data:([A-Za-z-+/]+);base64,(.+)$/),
     response = {};
 
@@ -141,7 +142,6 @@ function saveImage(picture, dir) {
     if (err) return console.error(err);
     console.log("file saved to ", `${dir}/${fileName}`);
   });
-  // link.click();
 }
 
 //Recording
